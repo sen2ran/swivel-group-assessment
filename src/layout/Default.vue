5a30a47f5740ffb8624b0f23d2ec7888fa933119
Senthuran <senthu1993@gmail.com>
2021-08-02 09:02:03 +0530

    Layout added

---
+++ src/layout/Default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-0">
+    <page-header />
+    <section class="max-w-6xl  mx-auto px-8 my-8 h-screen">
+      <slot></slot>
+    </section>
+    <page-footer />
+  </div>
+</template>
+
+<script>
+import PageHeader from "../components/Common/PageHeader.vue";
+import PageFooter from "../components/Common/PageFooter.vue";
+export default {
+  components: {
+    PageHeader,
+    PageFooter,
+  },
+};
+</script>
