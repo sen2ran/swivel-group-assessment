f0368f25d3c8bfeac713ceabafd0e228fcfeee00
Senthuran <senthu1993@gmail.com>
2021-08-02 10:31:05 +0530

    Detail added

---
+++ src/components/User/ActiveTag.vue
@@ -0,0 +1,13 @@
+<template>
+  <span
+    class="text-sm font-medium py-1 px-2 rounded align-middle m-2"
+    :class="state ? 'text-green-500 bg-green-100' : 'bg-red-100 text-red-500'"
+    >{{ name }}     
+  </span>
+</template>
+
+<script>
+export default {
+  props: ["name", "state"],
+};
+</script>
