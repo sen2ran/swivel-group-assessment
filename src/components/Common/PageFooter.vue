a9489c0b6d4b673de7455d4e2c38265f49e051dc
Senthuran <senthu1993@gmail.com>
2021-08-02 08:59:08 +0530

    Header and footer added

---
+++ src/components/Common/PageFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="lg:py-0 bg-red-800 text-white shadow-md sticky lg:top-0 z-1000">
+    <div class="max-w-6xl  mx-auto px-8 flex flex-wrap items-center">
+      <div class="w-full items-center">
+        <div class="flex items-center w-auto ml-auto">
+          <ul class="lg:flex justify-between text-md text-white font-semibold">
+            <li class="flex">
+              <button
+                class="flex items-center py-3 px-4  hover:text-gray-500 font-semibold bg-primary-darker"
+              >
+                Page Footer
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Common/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header
+    class="lg:py-0 bg-red-800 text-white shadow-md  lg:top-0 z-10000"
+  >
+    <div class="max-w-6xl  mx-auto px-8 flex flex-wrap items-center">
+      <div class="w-full items-center">
+        <div class="flex items-center w-auto ml-auto">
+          <ul class="lg:flex justify-between text-md text-white font-semibold">
+            <li class="flex" to="/about">
+              <button
+                class="flex items-center py-3 px-4  hover:text-gray-500 font-semibold bg-primary-darker"
+              >
+                Page Header
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
